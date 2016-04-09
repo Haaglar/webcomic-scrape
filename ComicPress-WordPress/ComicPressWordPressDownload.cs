ff315af45b3d4e58fc0167450f1608454b68e1ac
Haaglar <ohaag63@gmail.com>
2016-04-09 13:10:37 +1000

    Started to use HTMLAglitiy pack, and XPath learning. Gets first path URL

--- ComicPress-WordPress/ComicPressWordPressDownload.cs
+++ ComicPress-WordPress/ComicPressWordPressDownload.cs
@@ -1,7 +1,8 @@
-﻿using System;
-
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
 using System.Text;
-
+using System.Text.RegularExpressions;
 
 namespace ComicPress_WordPress
 {
@@ -14,12 +15,24 @@ namespace ComicPress_WordPress
             {
                 return (int)ExitCodes.InvalidArguments;
             }
-            Uri asd = new Uri(args[0]);
+            string url = args[0].StartsWith("http") ? args[0] : "http://" + args[0];
             //Console.WriteLine(asd.GetLeftPart(UriPartial.Authority));
 
             //Looking at ComicPress webcomics (5) i see two different versions on how the pages navigate
             //ones that use comic-nav-base as a base and others that use navi navi as a base
+            //And some dont have a first in the noraml place
             //TODO: handle them both :)  
+            string xpathComic = "//*[@id=\"comic\"]/img";
+
+            //Need to find first
+            string xpathFirst = "//*[@class=\"navi navi-first\" or @class=\"comic-nav-base comic-nav-first\"]";
+            HtmlDocument webPagestart = new HtmlWeb().Load(url);
+            var nodes = webPagestart.DocumentNode.SelectNodes(xpathFirst);
+            if(nodes == null)
+            {
+                return (int)ExitCodes.InvalidSite;
+            }
+            Console.WriteLine(nodes[0].Attributes["href"].Value);
             return (int)ExitCodes.Success;
         }
     }
