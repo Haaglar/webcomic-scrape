f795ec7e352503ff22f80151adfd5cc2023bb114
Haaglar <ohaag63@gmail.com>
2016-04-11 16:23:03 +1000

    Begun work on a SmackJeeves scraper

---
+++ SmackJeeves/SmackJeevesDownload.cs
@@ -0,0 +1,23 @@
+﻿using System;;
+
+
+namespace SmackJeeves
+{
+    class SmackJeevesDownload
+    {
+        enum ExitCodes { Success, InvalidArguments, InvalidSite };
+        static int Main(string[] args)
+        {
+            int comicNo = 0;
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: SmackJeeves.exe URL");
+                return (int)ExitCodes.InvalidArguments;
+            }
+
+            string xPathNavigation = "//*[@class=\"navbar\"]//a";
+            string xPathImage = "//*[@id=\"comic_image\"]";
+            return (int)ExitCodes.Success;
+        }
+    }
+}
