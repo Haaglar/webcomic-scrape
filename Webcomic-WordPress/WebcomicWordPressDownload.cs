9f81901607697b87ef2ba2c61952207a01651109
Haaglar <ohaag63@gmail.com>
2016-09-10 10:12:17 +1000

    Updated the Webcomicwordpress application to use xpath instead of regex

--- Webcomic-WordPress/WebcomicWordPressDownload.cs
+++ Webcomic-WordPress/WebcomicWordPressDownload.cs
@@ -1,20 +1,16 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.IO;
 using System.Net;
-using System.Text.RegularExpressions;
 
 namespace Webcomic_WordPress
 {
     class WebcomicWordPressDownload
     {
-        enum ExitCodes { Success, InvalidArguments, InvalidSite, LostConnection };
-        //Webcomic works by having a div with the class "webcomic full" that contains the current webcomic and controls
-        //Unfortunatly the image is stored in a dynamically named class so regex it is :^)
-
+        enum ExitCodes { Success, InvalidArguments, InvalidSite, LostConnection, PageError };
         static int Main(string[] args)
         {
             bool indexing = false; //Name based on comic number upload
-            string indexingTempFileLocation = "";
             if (args.Length <1 || args.Length > 2)
             {
                 Console.WriteLine("Usage: Webcomic-wordpress.exe [-i] URL");
@@ -24,116 +20,83 @@ namespace Webcomic_WordPress
             {
                 indexing = true;   
             }
-            Uri address;
+            HtmlDocument webPagestart;
+            string address;
             try
             {
-                address = new Uri(args[args.Length-1].StartsWith("http") ? args[args.Length - 1] : "http://" + args[args.Length - 1]);
+                address = args[args.Length - 1].StartsWith("http") ? args[args.Length - 1] : "http://" + args[args.Length - 1];
+                webPagestart = new HtmlWeb().Load(address);
             }
             catch
             {
-                Console.WriteLine("Invalid URL");
+                Console.WriteLine("Failed to download from URL");
                 Console.WriteLine("Usage: Webcomic-wordpress.exe [-i] URL");
                 return (int)ExitCodes.InvalidArguments;
             }
-            Console.WriteLine("Trying to download from " + address.AbsoluteUri);
-            String initialPage;
-            using (WebClient initialRequest = new WebClient())
-            {
-                try
-                {
-                    initialPage = initialRequest.DownloadString(address);
-                }
-                catch (WebException)
-                {
-                    Console.WriteLine("Invalid URL / Couldn't reach host");
-                    Console.WriteLine("Usage: Webcomic-wordpress.exe [-i] URL");
-                    return (int)ExitCodes.InvalidArguments;
-                }
-            }
 
             //Need to create a folder
-            string folderDir = address.AbsoluteUri;
+            string folderDir = address  ;
             foreach (var c in Path.GetInvalidFileNameChars())
             {
                 folderDir = folderDir.Replace(c, '-');
             }
             Directory.CreateDirectory(folderDir);
-            //Finds the webcomic image
-            Regex imageFind = new Regex(@"<img\s*?src=""([^""]*)""\s*?width=""\d*""\s*?height=""\d*""");
-            //Gets the link to the first page
-            Regex findFirstPage = new Regex(@"<a\s*?href=""([^""]*)""\s*?rel=""first""");
-            //Finds the last page
-            Regex findLastPage = new Regex(@"href=""([^""]*)""\s*?rel=""last""\s*?class=""last-webcomic-link last-webcomic-link");
-            //Finds the next page
-            Regex findNextPage = new Regex(@"href=""([^""]*)""\s*?rel=""next""\s*?class=""next-webcomic-link next-webcomic-link");
+            //XPATH
+            //path to image
+            string imageXPath = "//span/img";
+            //path tofirst page
+            string firstXPath = "//a[@rel=\"first\"]";
+            //path to next page
+            string nextXPath = "//*[@rel=\"next\"]";
+            //Path to final, as once we reach here we make no more requests
+            string lastXPath = "//*[@rel=\"last\"]";
+
             //Now do the hard work
 
-            //Initial page crawl as we have to do a couple additional things
-            Match m1 = imageFind.Match(initialPage);
-            Match lastPageUrl = findLastPage.Match(initialPage);
-            if (m1.Length == 0 || lastPageUrl.Length==0)
+            //Initial page crawl as we have to do a couple additional things, like finding the first page
+            HtmlNodeCollection nodesFirst = webPagestart.DocumentNode.SelectNodes(firstXPath);
+            HtmlNodeCollection nodesLast = webPagestart.DocumentNode.SelectNodes(lastXPath);
+            if (nodesFirst == null || nodesLast == null)
             {
                 Console.WriteLine("Site doesn't use Webcomic");
                 Console.WriteLine("Usage: Webcomic-wordpress.exe [-i] URL");
                 return (int)ExitCodes.InvalidSite;
             }
-            string url = m1.Groups[1].Value;
-            string finalurl = lastPageUrl.Groups[1].Value;
-            int pageNo = 1;
-            using (WebClient wc = new WebClient())
+
+            string currentPage = nodesFirst[0].Attributes["href"].Value;
+            string finalurl = nodesLast[0].Attributes["href"].Value;
+            int pageNo = 0;
+
+            //Start crawl loop 
+            do
             {
-                string fileName;
-                if (indexing)
+
+                HtmlDocument currentDocument = new HtmlWeb().Load(currentPage);
+                HtmlNodeCollection imgLoc = currentDocument.DocumentNode.SelectNodes(imageXPath);
+                if (imgLoc != null) //May not have an image
                 {
-                    fileName = folderDir + "/" + "temp" + Path.GetExtension(url);
-                    indexingTempFileLocation = fileName;
+                    string currentPageImageURL = imgLoc[0].Attributes["src"].Value;
+                    using (WebClient wc = new WebClient())
+                    {
+                        //Filename based on user option
+                        string fileName = indexing ? folderDir + "/" + (++pageNo).ToString("0000") + Path.GetExtension(currentPageImageURL) : folderDir + "/" + Path.GetFileName(currentPageImageURL);
+                        wc.DownloadFile(currentPageImageURL, fileName);
+                        Console.WriteLine("Downloaded: " + Path.GetFileName(fileName));
+                    }
                 }
                 else
                 {
-                    fileName = folderDir + "/" + Path.GetFileName(url);
+                    Console.WriteLine("Image not found at " + currentPage);
                 }
-                wc.DownloadFile(url, fileName);
-                Console.WriteLine("Downloaded: " + Path.GetFileName(fileName));
-            }
-            string currentPage = findFirstPage.Match(initialPage).Groups[1].Value;
-            //Start crawl loop 
-            while (!currentPage.Equals(finalurl))
-            {
-                string thisPage;
-                using (WebClient wc = new WebClient())
-                {
-                    try
-                    {
-                        thisPage = wc.DownloadString(currentPage);
-                    }
-                    catch (WebException)
-                    {
-                        Console.WriteLine("Couldn't reach host");
-                        return (int)ExitCodes.InvalidArguments;
-                    }
-                }
-                Match currentPageImage = imageFind.Match(thisPage);
-                Match nextPageUrl = findNextPage.Match(thisPage);
-                string currentPageImageURL = currentPageImage.Groups[1].Value;
 
-                using (WebClient wc = new WebClient())
+                HtmlNodeCollection nextLoc = currentDocument.DocumentNode.SelectNodes(nextXPath);
+                if (nextLoc == null)
                 {
-                    string fileName = indexing ? folderDir + "/" + pageNo.ToString("0000") + Path.GetExtension(currentPageImageURL) : folderDir + "/" + Path.GetFileName(currentPageImageURL);
-                    pageNo++;
-                    wc.DownloadFile(currentPageImageURL, fileName);
-                    Console.WriteLine("Downloaded: " + Path.GetFileName(fileName));
+                    Console.WriteLine("Could not find next page link at " + currentPage);
+                    return (int)ExitCodes.PageError;
                 }
-                if (nextPageUrl.Length == 0)// At the end or something  
-                {
-                    break;
-                }
-                currentPage = nextPageUrl.Groups[1].Value;
-            }
-            if (indexing)
-            {
-                Console.WriteLine("Renaming temp file");
-                File.Move(indexingTempFileLocation, folderDir + "/" + pageNo.ToString("0000") + Path.GetExtension(indexingTempFileLocation));
-            }
+                currentPage = nextLoc[0].Attributes["href"].Value;
+            } while (!currentPage.Equals(finalurl)); //We use do while to handle the case of 1 page 
             Console.WriteLine("Complete");
             return (int)ExitCodes.Success;
         }
